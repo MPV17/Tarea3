8b8ecbbd9e42caf4a0b61fae74a44443522a9149
NeekoSakata <114708095+NeekoSakata@users.noreply.github.com>
2022-11-16 03:20:55 -0300

    Update Deposito.java

--- Deposito.java
+++ Deposito.java
@@ -1,3 +1,5 @@
+package tarea3;
+import java.awt.Graphics;
 import java.util.ArrayList;
 //"Almacen"
 
@@ -8,11 +10,11 @@ class Deposito{
     }
     public void addBebida(Bebida b1){
         almacen.add(b1);
-
+        
     }
     public Bebida getBebida(){
         if (almacen.size()!=0) {
-            Bebida b2 = almacen.remove(0);
+            Bebida b2 = almacen.remove(almacen.size()-1);
             return b2;
         }else return null;
     }
@@ -21,6 +23,25 @@ class Deposito{
     }
     public int getStock(){
         return almacen.size();
+    }
+    public void paint(Graphics g){
+        int x = 80;
+        int y = 95;
+        for(int i=0;i<almacen.size();i++){
+            switch(almacen.get(0).getSerie()){
+            case 100: 
+                x=80;
+                break;
+            case 200:
+                x=230;
+                break;
+            case 300:
+                x=380;
+                break;
+        }
+        almacen.get(i).paint(g, x, y);
+        y=y-5;
+        }
     }
 }
 //"Almacen"
