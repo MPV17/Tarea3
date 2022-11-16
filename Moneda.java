35d78b4160b13b5c876d9621fa5edddc47a9ef54
NeekoSakata <114708095+NeekoSakata@users.noreply.github.com>
2022-11-16 03:24:04 -0300

    Update Moneda.java

--- Moneda.java
+++ Moneda.java
@@ -1,3 +1,5 @@
+package tarea3;
+
 //Monedas
 
 import java.awt.Color;
@@ -5,57 +7,62 @@ import java.awt.Graphics;
 
 class Moneda{
     public Moneda(){
-
+        
     }
     public int getValor(){
         return 0;
     }
-    public void paint (Graphics g){
+    public void paint (Graphics g,int x, int y){
         g.setColor(Color.yellow);
-        g.fillOval(50,100,100,100); //o llamar a paint de objetos creados
-
+        
+       
     }
 }
 class Moneda100 extends Moneda{
     public Moneda100(){
-
+        
     }
     public int getValor(){
         return super.getValor() + 100;
     }
-    public void paint (Graphics g){
-        super.paint(g);
+    public void paint (Graphics g,int x, int y){
+        super.paint(g,x,y);
         g.setColor(Color.green);
-        g.fillOval(50,100,100,100); //o llamar a paint de objetos creados
-
+        g.fillOval(x,y,75,75); //o llamar a paint de objetos creados
+        g.setColor(Color.BLACK);
+        g.drawOval(x,y,75,75);
     }
 }
 class Moneda500 extends Moneda{
     public Moneda500(){
-
+        
     }
     public int getValor(){
         return super.getValor() + 500;
     }
-    public void paint (Graphics g){
-        super.paint(g);
+    public void paint (Graphics g,int x, int y){
+        super.paint(g,x,y);
         g.setColor(Color.red);
-        g.fillOval(50,100,100,100); //o llamar a paint de objetos creados
-
+        g.fillOval(x,y,75,75); //o llamar a paint de objetos creados
+        g.setColor(Color.BLACK);
+        g.drawOval(x,y,75,75);
     }
 }
 class Moneda1000 extends Moneda{
     public Moneda1000(){
-
+        
     }
     public int getValor(){
         return super.getValor() + 1000;
     }
-    public void paint (Graphics g){
-        super.paint(g);
+    public void paint (Graphics g,int x, int y){
+        super.paint(g,x,y);
         g.setColor(Color.blue);
-        g.fillOval(50,100,100,100); //o llamar a paint de objetos creados
-
+        g.fillOval(x,y,75,75); //o llamar a paint de objetos creados
+        g.setColor(Color.BLACK);
+        g.drawOval(x,y,75,75);
+        //g.fillRect(x, y, y, y);
+       
     }
 }
 //Monedas
