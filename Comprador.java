a7087045505ab7d21b02b0fe333717a522d8fc37
NeekoSakata <114708095+NeekoSakata@users.noreply.github.com>
2022-11-16 03:19:56 -0300

    Update Comprador.java

--- Comprador.java
+++ Comprador.java
@@ -1,3 +1,5 @@
+package tarea3;
+
 //Comprador
 
 import java.awt.Color;
@@ -7,20 +9,20 @@ class Comprador{
     private String queBebiste;
     private int cuantoVuelto;
     public Comprador(Moneda m ,int seleccion , Expendedor maquina){
-        this.cuantoVuelto=0;
+        /*this.cuantoVuelto=0;
         try{
             Bebida mibebida = maquina.comprarBebida(m, seleccion);
             queBebiste =  mibebida.beber();
             while(maquina.getCuentaDeVuelto()!=0){
                 cuantoVuelto = cuantoVuelto + (maquina.getVuelto()).getValor();
             }
-
-
+            
+            
         }catch(NoHayBebidaException | PagoIncorrectoException | PagoInsuficienteException e){
             System.out.println(e.getMessage());
-        }
+        }*/
     }
-
+    
     public String getQueBebiste(){
         return queBebiste;
     }
@@ -28,9 +30,13 @@ class Comprador{
         return cuantoVuelto;
     }
     public void paint (Graphics g){
-        g.setColor(Color.blue);
-        g.fillOval(100,100,100,100); //o llamar a paint de objetos creados
-
+        g.setColor(Color.orange); //o llamar a paint de objetos creados
+        g.fillOval(1150,50,200,200);
+        g.fillOval(1150,249,200,400);
+        g.fillOval(1000,300,200,75);
+        g.fillOval(1300,300,200,75);
+        g.fillOval(1150,600,75,200);
+        g.fillOval(1275,600,75,200);
     }
 }
 //Comprador
